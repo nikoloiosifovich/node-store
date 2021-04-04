fd0951414b88ab7d07afe1077b761fe2b67bfcd5
Nikolo Iosifovich <vieiraneto88@gmail.com>
2021-04-03 22:42:18 -0300

    feat: basic CRUD sctructure

--- src/routes.js
+++ src/routes.js
@@ -9,4 +9,29 @@ appRoutes.get('/', (req, res) => {
   })
 })
 
+appRoutes.post('/', (req, res) => {
+  const { id, title } = req.body
+
+  return res.status(201).json({
+    id,
+    title
+  })
+})
+
+appRoutes.put('/:id', (req, res) => {
+  const { id } = req.params
+  const { title } = req.body
+
+  return res.json({
+    id,
+    title
+  })
+})
+
+appRoutes.delete('/:id', (req, res) => {
+  return res.json({
+    message: 'Deleted!'
+  })
+})
+
 export { appRoutes }
