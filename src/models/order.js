af19a10e3e912ad1104a46c5542cb5550ea9272c
Nikolo Iosifovich <vieiraneto88@gmail.com>
2021-04-17 21:18:46 -0300

    feat: ensure Order model schema

---
+++ src/models/order.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+const { Schema, model } = mongoose
+
+const productSchema = new Schema(
+  {
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Customer'
+    },
+    number: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    createDate: {
+      type: Date,
+      required: true,
+      default: Date.now
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['created', 'done'],
+      default: 'created'
+    },
+    items: [{
+      quantidade: {
+        type: Number,
+        required: true,
+        default: 1
+      },
+      price: {
+        type: Number,
+        required: true
+      },
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+      }
+    }]
+  },
+  {
+    timestamps: true
+  }
+)
+
+export default model('Order', productSchema)
