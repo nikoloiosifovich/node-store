bf64846e6708d41d6a5aa08e85c51c1d3f24a986
Nikolo Iosifovich <vieiraneto88@gmail.com>
2021-04-18 05:06:33 -0300

    feat: populate orderRepository.get with customer and product info

--- src/repositories/orderRepository.js
+++ src/repositories/orderRepository.js
@@ -2,7 +2,10 @@ import Order from '../models/order.js'
 import { v4 as uuidv4 } from 'uuid'
 
 const orderRepository = () => ({
-  get: () => Order.find(),
+  get: () => Order
+    .find({}, 'number status customer items')
+    .populate('customer', 'name')
+    .populate('items.product', 'title'),
 
   create: (order) => {
     Object.assign(order, {
