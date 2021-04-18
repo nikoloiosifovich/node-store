66d8fd7154c7c89ed4134d444cf5f17ed1784014
Nikolo Iosifovich <vieiraneto88@gmail.com>
2021-04-18 05:31:54 -0300

    feat: password hash as md5

--- src/repositories/customerRepository.js
+++ src/repositories/customerRepository.js
@@ -1,9 +1,14 @@
 import Customer from '../models/customer.js'
+import md5 from 'md5'
 
 const customerRepository = () => ({
   get: () => Customer.find(),
 
-  create: (customer) => Customer.create(customer)
+  create: (customer) => {
+    customer.password = md5(customer.password + process.env.SALT_KEY)
+
+    return Customer.create(customer)
+  }
 })
 
 export default customerRepository()
